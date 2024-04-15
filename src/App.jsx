11ed066d2827cbe9954ecd1b2a2ea5b25289b2c0
Shahzaib Ahmed <161369939+ShahzaibAhmet@users.noreply.github.com>
2024-04-16 03:49:32 +0500

    Loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { useState } from 'react';
+import Background from "./components/Background"
+import Hero from "./components/Hero"
+import Navbar from "./components/Navbar"
+
 
 const App = () => {
+  let HeroData = [
+    {text1:"Dive into", text2:"what you love"},
+    {text1:"Indulge", text2:"your passions"},
+    {text1:"Give in to", text2:"your passions"},
+  ];
+const [HeroCount, setHeroCount] = useState(2);
+const [PlayStatus, setPlayStatus] = useState(false);
+
   return (
-    <div>
-      
-    </div>
+    <>
+  
+    </>
   )
 }
 
